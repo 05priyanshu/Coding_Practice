260058cb369d7b31097254ba9aaa378c4711266a
PRIYANSHU_24 <118009761+05priyanshu@users.noreply.github.com>
2023-10-18 23:42:55 +0530

    Added solution - LeetHub

---
+++ Remove duplicates from an unsorted linked list - GFG/remove-duplicates-from-an-unsorted-linked-list.cpp
@@ -0,0 +1,106 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+
+void print(Node *root)
+{
+Node *temp = root;
+while(temp!=NULL)
+{
+cout<<temp->data<<" ";
+temp=temp->next;
+}
+}
+
+
+// } Driver Code Ends
+/*
+The structure of linked list is the following
+
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+*/
+
+
+class Solution
+{
+    public:
+    //Function to remove duplicates from unsorted linked list.
+    // void delete_node(Node* head,Node*pre,Node*NEXT){
+    //     pre->next=NEXT;
+    // }
+    Node * removeDuplicates( Node *head) 
+    {
+        unordered_map<int,int> map;
+        Node* start=head;
+        Node* pre=NULL;
+        while(start!=NULL){
+            if(map[start->data]==0){
+                map[start->data]++;
+                pre=start;
+            }
+            else{
+                // delete_node(start,pre,NEXT);
+                pre->next=start->next;
+                delete start;
+                
+            }
+            start=start->next;
+            // NEXT=start->next;
+            
+        }
+        return head;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	
+	while(T--)
+	{
+		int K;
+		cin>>K;
+		struct Node *head = NULL;
+        struct Node *temp = head;
+ 
+		for(int i=0;i<K;i++){
+		int data;
+		cin>>data;
+			if(head==NULL)
+			head=temp=new Node(data);
+			else
+			{
+				temp->next = new Node(data);
+				temp=temp->next;
+			}
+		}
+		
+	    Solution ob;
+		Node *result  = ob.removeDuplicates(head);
+		print(result);
+		cout<<endl;
+		
+	}
+	return 0;
+}
+// } Driver Code Ends
