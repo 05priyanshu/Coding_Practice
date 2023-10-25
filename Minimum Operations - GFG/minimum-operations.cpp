f23119425325ad1910d5c39812ddc896a91d5918
PRIYANSHU_24 <118009761+05priyanshu@users.noreply.github.com>
2023-10-26 02:12:10 +0530

    Added solution - LeetHub

--- Minimum Operations - GFG/minimum-operations.cpp
+++ Minimum Operations - GFG/minimum-operations.cpp
@@ -9,18 +9,7 @@ class Solution
     int minOperation(int n)
     {
         int count=0;
-        // int ans=1;
-        while(n!=0){
-            if(n%2==0){
-                n=n/2;
-                count++;
-            }
-            else{
-                n=n-1;
-                count++;
-            }
-        }
-        
+        while(n!=0){ (n%2==0)?n=n/2:n=n-1; count++; }
         return count;
     }
 };
